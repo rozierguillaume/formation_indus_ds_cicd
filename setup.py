b0158ebcebe9a1bcbb400b5750ea7330258b6efc
eltoulemonde <toul@octo.com>
2018-12-17 16:30:13 +0100

    Create setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='formation_indus_ds',
+    version='0.1.0',
+    packages=['src'],
+    url='',
+    license='',
+    author='Octo-TOUL',
+    author_email='',
+    description='Demonstration pour la formation indus de la data science'
+)
